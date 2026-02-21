e9ac4e0b457b78df21eda0c0ed050f79853d20cc
k <ksmith1489@protonmail.com>
2026-02-21 17:47:05 -0500

    Update octogoat.js

--- octoprint_octogoat/static/js/octogoat.js
+++ octoprint_octogoat/static/js/octogoat.js
@@ -1,112 +1,32 @@
-$(function () {
+$(function() {
 
     function OctoGoatViewModel(parameters) {
         var self = this;
 
-        self.settingsViewModel = parameters[0];
-
-        // ---- Safe access pattern ----
-        self.pluginSettings = function () {
-            return self.settingsViewModel.settings.plugins.octoprint_octogoat;
-        };
+        self.settings = parameters[0];
 
         self.confirmed = ko.observable(false);
 
-        self.freeResumesRemaining = ko.observable(0);
-        self.licenseValid = ko.observable(false);
-
-        self.onBeforeBinding = function () {
-            var ps = self.pluginSettings();
-
-            self.freeResumesRemaining(ps.free_resumes_remaining());
-            self.licenseValid(ps.cached_valid());
-        };
-
-        // -----------------------------
-        // ANALYZE
-        // -----------------------------
-        self.analyze = function () {
-            OctoPrint.simpleApiCommand("octoprint_octogoat", "analyze", {
-                firmware: "klipper",
-                mph: 10,      // temporary test values
-                lh: 0.2
-            }).done(function (response) {
-
-                if (!response.ok) {
+        self.resumePrint = function() {
+            OctoPrint.simpleApiCommand("octoprint_octogoat", "resume", {})
+                .done(function(response) {
                     new PNotify({
-                        title: "Analyze Error",
-                        text: response.error || "Unknown error",
+                        title: "Resume",
+                        text: "Resume triggered.",
+                        type: "success"
+                    });
+                })
+                .fail(function() {
+                    new PNotify({
+                        title: "Error",
+                        text: "Resume failed.",
                         type: "error"
-                    });
-                    return;
-                }
-
-                new PNotify({
-                    title: "Analyze Success",
-                    text: "Resume height: " + response.resume_z,
-                    type: "success"
-                });
-
-            }).fail(function () {
-                new PNotify({
-                    title: "Server Error",
-                    text: "Analyze failed",
-                    type: "error"
-                });
-            });
-        };
-
-        // -----------------------------
-        // SAVE ONLY
-        // -----------------------------
-        self.saveResume = function () {
-            OctoPrint.simpleApiCommand("octoprint_octogoat", "save_resume_file", {})
-                .done(function (response) {
-
-                    if (!response.ok) {
-                        new PNotify({
-                            title: "Error",
-                            text: response.error,
-                            type: "error"
-                        });
-                        return;
-                    }
-
-                    self.freeResumesRemaining(response.free_remaining);
-
-                    new PNotify({
-                        title: "Saved",
-                        text: response.filename,
-                        type: "success"
-                    });
-                });
-        };
-
-        // -----------------------------
-        // RESUME NOW
-        // -----------------------------
-        self.resumeNow = function () {
-            OctoPrint.simpleApiCommand("octoprint_octogoat", "resume_now", {})
-                .done(function (response) {
-
-                    if (!response.ok) {
-                        new PNotify({
-                            title: "Error",
-                            text: response.error,
-                            type: "error"
-                        });
-                        return;
-                    }
-
-                    new PNotify({
-                        title: "Printing",
-                        text: "Resume started",
-                        type: "success"
                     });
                 });
         };
     }
 
+    // IMPORTANT: Do NOT wrap this in any extra closures
     OCTOPRINT_VIEWMODELS.push([
         OctoGoatViewModel,
         ["settingsViewModel"],
