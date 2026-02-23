43009ec509b2f9a56987ed0938c2529a06bb4c29
k <ksmith1489@protonmail.com>
2026-02-23 15:42:48 -0500

    testconnection not defined save button spinning debug

--- octoprint_octogoat/static/js/octogoat.js
+++ octoprint_octogoat/static/js/octogoat.js
@@ -1,9 +1,10 @@
 $(function () {
-
     function OctoGoatViewModel(parameters) {
         var self = this;
 
-        self.settings = parameters[0];
+        self.settingsViewModel = parameters[0];
+        self.settings = self.settingsViewModel.settings;   // <-- IMPORTANT
+
         self.confirmed = ko.observable(false);
 
         self.testConnection = function () {
@@ -12,21 +13,19 @@ $(function () {
                     alert(JSON.stringify(resp, null, 2));
                 })
                 .fail(function (jqXHR) {
-                    var payload = jqXHR.responseJSON || {
-                        error: jqXHR.responseText || "Request failed"
-                    };
+                    var payload = jqXHR.responseJSON || { error: jqXHR.responseText || "Request failed" };
                     alert(JSON.stringify(payload, null, 2));
                 });
         };
 
         self.resumePrint = function () {
-            alert("Resume clicked (stub).");
+            alert("Resume clicked (stub). Next step: wire this to build_resumed_gcode + job creation.");
         };
     }
 
     OCTOPRINT_VIEWMODELS.push({
         construct: OctoGoatViewModel,
-        dependencies: ["settingsViewModel"]
+        dependencies: ["settingsViewModel"],
+        elements: ["#settings_plugin_octogoat", "#tab_plugin_octogoat"]
     });
-
 });
