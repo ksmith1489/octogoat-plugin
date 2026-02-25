02c415b97109cb70a812bc4e58da9358680b8332
k <ksmith1489@protonmail.com>
2026-02-24 20:22:58 -0500

    Update octogoat.js

--- octoprint_octogoat/static/js/octogoat.js
+++ octoprint_octogoat/static/js/octogoat.js
@@ -12,13 +12,48 @@ $(function () {
                 .done(function (resp) {
                     alert(JSON.stringify(resp, null, 2));
                 })
-                .fail(function (jqXHR) {
+                .fail(function () {
                     alert("Request failed");
                 });
         };
 
         self.resumePrint = function () {
             alert("Resume clicked (stub).");
+        };
+
+        self.licenseValid = ko.observable(false);
+
+        self.validateLicense = function () {
+
+            var key = self.settingsViewModel.settings.plugins.octogoat.api_key();
+
+            if (!key) {
+                self.licenseValid(false);
+                return;
+            }
+
+            $.ajax({
+                url: "https://app.lazarus3dprint.com/validate",
+                method: "POST",
+                contentType: "application/json",
+                data: JSON.stringify({
+                    license_key: key
+                }),
+                success: function (response) {
+                    self.licenseValid(response.valid === true);
+                },
+                error: function () {
+                    self.licenseValid(false);
+                }
+            });
+        };
+
+        self.openCheckout = function () {
+            window.open("https://lazarus3dprint.com/checkout", "_blank");
+        };
+
+        self.onBeforeBinding = function () {
+            self.validateLicense();
         };
     }
 
